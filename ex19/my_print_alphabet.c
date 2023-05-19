354e48a549145dcc217ddea599b79d89c5b84f05
Khadicha <246abdurashidova123@gmail.com>
2023-05-20 00:57:00 +0500

    ex19 finished

---
+++ ex19/my_print_alphabet.c
@@ -0,0 +1,12 @@
+void my_putchar(char c) {
+  write(1, &c, 1);
+}
+
+void my_print_alphabet()
+{
+for (char letter = 'a'; letter <= 'z'; letter++ )
+    {
+     my_putchar(letter);
+    }
+    my_putchar('\n');
+}
